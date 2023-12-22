227bf8b6637679d3bed023cbb6d1a5eb244f5de7
Yarolsav <kiritok712@gmail.com>
2023-12-22 11:05:04 +0100

    update_pizza

---
+++ script/JS/main.js
@@ -0,0 +1,23 @@
+const cartBtn = document.querySelector('.btn2');
+const korzina = document.querySelector('.korzina');
+const close = document.querySelector('.close');
+// cartBtn.addEventListener("click", toggleModal);
+// close.addEventListener("click", toggleModal);
+
+cartBtn.addEventListener("click", function (event) {
+    korzina.classList.add("is-open");
+})
+
+close.addEventListener("click", function (event) {
+    korzina.classList.remove("is-open");
+})
+
+// function toggleModal(){
+//     korzina.classList.toogle("is-open");
+// }
+
+new WOW().init();
+
+
+
+console.log("hellp");
